54e0ae17c7c5d63a3df606c45408e35c035cbe6a
RobsonWebDev <robsonwebpro@gmail.com>
2023-08-11 11:36:56 -0300

    Adicionando algumas configuracoes no admin

--- contact/admin.py
+++ contact/admin.py
@@ -3,4 +3,8 @@ from contact.models import Contact
 
 @admin.register(Contact)
 class ContactAdmin(admin.ModelAdmin):
-    ...
+    list_display = 'id', 'first_name', 'last_name', 'phone',
+    ordering = '-id',
+    search_fields = 'id', 'first_name', 'last_name',
+    list_per_page = 10
+    list_max_show_all = 100
