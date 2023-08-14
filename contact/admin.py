25ad19a512ae1b5ead877979433f67dba3667c3f
RobsonWebDev <robsonwebpro@gmail.com>
2023-08-14 09:20:22 -0300

    Criando mais uma model com ForeignKey e config no Admin

--- contact/admin.py
+++ contact/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from contact.models import Contact
+from contact.models import Contact, Category
 
 @admin.register(Contact)
 class ContactAdmin(admin.ModelAdmin):
@@ -8,3 +8,8 @@ class ContactAdmin(admin.ModelAdmin):
     search_fields = 'id', 'first_name', 'last_name',
     list_per_page = 10
     list_max_show_all = 100
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = 'id', 'name',
+    ordering = '-id',
