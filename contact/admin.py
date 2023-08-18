eaeab8f29595f778024c9d53700580dbe36d2271
RobsonWebDev <robsonwebpro@gmail.com>
2023-08-17 21:43:20 -0300

    Criado links atraves do id e templates separados na apresentacao dos contatos

--- contact/admin.py
+++ contact/admin.py
@@ -3,9 +3,10 @@ from contact.models import Contact, Category
 
 @admin.register(Contact)
 class ContactAdmin(admin.ModelAdmin):
-    list_display = 'id', 'first_name', 'last_name', 'phone',
+    list_display = 'id', 'first_name', 'last_name', 'phone', 'show',
     ordering = '-id',
     search_fields = 'id', 'first_name', 'last_name',
+    list_editable = 'show',
     list_per_page = 10
     list_max_show_all = 100
 
