5ece34d5dd7e06fe9519f25e0248eabbd99aa647
RobsonWebDev <robsonwebpro@gmail.com>
2023-08-17 22:28:42 -0300

    algumas alteracoes no template

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -8,6 +8,7 @@ def index(request):
 
     context = {
         'contacts': contacts,
+        'site_title': 'Contatos - ',
     }
 
     return render(request, 'contact/index.html', context)
@@ -18,8 +19,11 @@ def contact(request, contact_id):
     if single_contact is None:
         raise Http404
 
+    site_title = f'{single_contact.first_name} {single_contact.last_name} - '
+
     context = {
         'contacts': single_contact,
+        'site_title': site_title,
     }
 
     return render(request, 'contact/contact.html', context)
