cf603b237e96dbc5d6f32fbbac2ad0dbe764bba8
RobsonWebDev <robsonwebpro@gmail.com>
2023-08-25 11:57:32 -0300

    Configurando create_forms.py

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -1,14 +1,31 @@
+from typing import Any, Dict
 from django.shortcuts import render, get_object_or_404, redirect
 from django.db.models import Q
+from django import forms
 from contact.models import Contact
 from django.core.paginator import Paginator
 
 
+class  ContactForm(forms.ModelForm):
+    class Meta:
+        model = Contact
+        fields = 'first_name','last_name', 'phone',
+
+    def clean(self):
+        cleaned_data = self.cleaned_data
+        
+        return super().clean()
+
 def create(request):
+    if request.method == 'POST':
+        context = {
+            'forms': ContactForm(request.POST)
+        }
+
+        return render(request, 'contact/create.html', context)
 
     context = {
-        'create': create,
-        'site_title': 'Create - ',
+        'forms': ContactForm()
     }
 
     return render(request, 'contact/create.html', context)
