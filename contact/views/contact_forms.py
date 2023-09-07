2136ad525fa36b2c4f178fc8132fc1f8f47e32d8
RobsonWebDev <robsonwebpro@gmail.com>
2023-09-06 22:58:40 -0300

    Algumas configuracoes no forms

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -1,20 +1,10 @@
 from typing import Any, Dict
 from django.shortcuts import render, get_object_or_404, redirect
 from django.db.models import Q
-from django import forms
 from contact.models import Contact
 from django.core.paginator import Paginator
+from contact.forms import ContactForm
 
-
-class  ContactForm(forms.ModelForm):
-    class Meta:
-        model = Contact
-        fields = 'first_name','last_name', 'phone',
-
-    def clean(self):
-        cleaned_data = self.cleaned_data
-        
-        return super().clean()
 
 def create(request):
     if request.method == 'POST':
