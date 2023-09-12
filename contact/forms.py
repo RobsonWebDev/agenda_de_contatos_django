ab57ee657934cb0e6051581fbcbfc2a6774577aa
RobsonWebDev <robsonwebpro@gmail.com>
2023-09-11 21:56:28 -0300

    Criando widget

--- contact/forms.py
+++ contact/forms.py
@@ -2,13 +2,23 @@ from django import forms
 from django.core.exceptions import ValidationError
 from . import models
 
-class  ContactForm(forms.ModelForm):
+class ContactForm(forms.ModelForm):
+    first_name = forms.CharField(
+        widget = forms.TextInput(
+            attrs = {
+                'placeholder' : 'Escreva aqui',
+            }
+        
+        ),
+        label = 'Primeiro Nome',
+        help_text = 'Texto de ajuda para o usuário',
+    )
+
     class Meta:
         model = models.Contact
         fields = 'first_name','last_name', 'phone',
 
     def clean(self):
-        cleaned_data = self.cleaned_data
         
         self.add_error(
             'first_name',
